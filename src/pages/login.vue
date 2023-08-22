ff88e729bcb041f62d73f19856e9ea7c1f4907d2
Ricards Taujenis <MozesThegreat@yahoo.com>
2023-08-22 22:09:59 +0200

    type feat -- login feature with Google
    login for both desktop and mobile display

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,25 +1,23 @@
 <template>
-    <main class="p-3 bg-color">
-        <template v-if="isMobile">
-        <MobileLoginLayout>
-          <NuxtLayout name="mobile-login"></NuxtLayout>
-        </MobileLoginLayout>
-        </template>
+  <main class="p-3 bg-color">
+    <template v-if="isMobile">
+      <MobileLoginLayout>
+        <NuxtLayout name="mobile-login"></NuxtLayout>
+      </MobileLoginLayout>
+    </template>
 
-      <template v-else>
-        <DesktopLoginLayout>
-          <NuxtLayout name="desktop-login"></NuxtLayout>
-        </DesktopLoginLayout>
-      </template>
-    </main>
-  </template>
-  
-  <script setup>
+    <template v-else>
+      <DesktopLoginLayout>
+        <NuxtLayout name="desktop-login"></NuxtLayout>
+      </DesktopLoginLayout>
+    </template>
+  </main>
+</template>
 
-  import { isMobile } from 'mobile-device-detect';
-  import MobileLoginLayout from '~/layouts/auth/mobile-login.vue'; 
-  import DesktopLoginLayout from '~/layouts/auth/desktop-login.vue';
-  
-  definePageMeta({ layout: 'auth/login' });
-  </script>
-  
+<script setup>
+import { isMobile } from 'mobile-device-detect';
+import MobileLoginLayout from '~/layouts/auth/mobile-login.vue';
+import DesktopLoginLayout from '~/layouts/auth/desktop-login.vue';
+
+definePageMeta({ layout: 'auth/login' });
+</script>
