c12fde4cafd069ac3b3d74ad1f0c2d4fb84baf51
Tanya Jain <tanya.jain27may@gmail.com>
2023-09-03 09:06:03 -0700

    feat(page): add subpages for profile section
    Signed-off-by: Tanya Jain <tanya.jain27may@gmail.com>
    Signed-off-by: owlot <95369289+owlot@users.noreply.github.com>

---
+++ src/pages/profile/privacy-and-security.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <Menu :activePageIndex="1"></Menu>
+        <hr class="m-1">
+        <h3> Profile Privacy and Security </h3>
+    </div>
+</template>
+
+<script setup>
+import Menu from '@/components/profile/menu.vue';
+definePageMeta({ layout: 'portal' });
+</script>
