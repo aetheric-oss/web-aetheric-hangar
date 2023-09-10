1cb49e0518bda73fbab430677e1aebb7772a2e69
Tanya Jain <tanya.jain27may@gmail.com>
2023-09-09 18:21:36 -0700

    feat(page): add profile settings base
    Signed-off-by: Tanya Jain <tanya.jain27may@gmail.com>
    Signed-off-by: owlot <95369289+owlot@users.noreply.github.com>

--- src/pages/profile/settings.vue
+++ src/pages/profile/settings.vue
@@ -2,11 +2,18 @@
     <div>
         <Menu :activePageIndex="0"></Menu>
         <hr class="m-1">
-        <h3> Profile Settings </h3>
+        <UserCard />
+        <hr class="m-1">
+        <ContactInfo />
+        <hr class="m-1">
+        <Addresses />
     </div>
 </template>
 
 <script setup>
 import Menu from '@/components/profile/menu.vue';
+import UserCard from '@/components/profile/settings/user-card.vue';
+import ContactInfo from '@/components/profile/settings/contact-info.vue';
+import Addresses from '@/components/profile/settings/addresses.vue';
 definePageMeta({ layout: 'portal' });
 </script>
