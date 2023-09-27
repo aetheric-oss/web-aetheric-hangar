a3366dd4bbfb6176b3629696987d1a34646a5d44
Tanya Jain <tanya.jain27may@gmail.com>
2023-09-27 11:18:09 -0700

    feat(component): add profile settings components
    Signed-off-by: Tanya Jain <tanya.jain27may@gmail.com>
    Signed-off-by: owlot <95369289+owlot@users.noreply.github.com>

---
+++ src/components/profile/settings/address-modal.vue
@@ -0,0 +1,163 @@
+<template>
+    <GenericModal
+        modalId="address-info-modal"
+        :title="displayTitle"
+        :handleSubmit="handleSubmit"
+        :submitDetails="submitDetails"
+        ref="addressModalRef"
+    >
+        <template #activation-button>
+            <slot></slot>
+        </template>
+        <template #content>
+            <form class="container">
+                <InputDropdown
+                    id="type"
+                    label="Type"
+                    type="text"
+                    :inputValue="selectedType"
+                    placeholder="Enter Type"
+                    @dropdownInput="onTypeInput"
+                    :items="types"
+                />
+                <InputText
+                    id="name"
+                    label="Name"
+                    type="text"
+                    :inputValue="form.name"
+                    placeholder="Enter Name"
+                    @input="form.name = $event"
+                />
+                <InputText
+                    id="address"
+                    label="Address Line"
+                    type="text"
+                    :inputValue="form.address.address"
+                    placeholder="Enter Address"
+                    @input="form.address.address = $event"
+                />
+                <InputText
+                    id="postalCode"
+                    label="Postal Code"
+                    type="text"
+                    :inputValue="form.address.postalCode"
+                    placeholder="Enter Postal Code"
+                    @input="form.address.postalCode = $event"
+                />
+                <InputText
+                    id="town"
+                    label="Town"
+                    type="text"
+                    :inputValue="form.address.town"
+                    placeholder="Enter Town"
+                    @input="form.address.town = $event"
+                />
+                <InputDropdown
+                    id="country"
+                    label="Country"
+                    type="text"
+                    :inputValue="selectedCountry"
+                    placeholder="Enter Country"
+                    @dropdownInput="onCountryInput"
+                    :items="countries"
+                />
+                <InputText
+                    id="state"
+                    label="State"
+                    type="text"
+                    :inputValue="form.address.state"
+                    placeholder="Enter State"
+                    @input="form.address.state = $event"
+                />
+            </form>
+        </template>
+    </GenericModal>
+</template>
+
+<script setup>
+import { computed, onMounted, ref, watch } from "vue";
+import { PhPencil } from "@phosphor-icons/vue";
+import GenericModal from '../../generic-modal.vue';
+import InputText from '@/components/input-fields/input-text.vue';
+import InputDropdown from "@/components/input-fields/input-dropdown.vue";
+const props = defineProps({
+    addressInfo: {
+        type: Object,
+        required: false
+    }
+});
+const displayTitle = computed(() => {
+    return props.contactInfo ? 'Edit Address Info' : 'Add Address Info';
+});
+let form = ref( props.addressInfo ? props.addressInfo : {
+    type: '',
+    name: '',
+    address: {
+        address: '',
+        postalCode: '',
+        town: '',
+        country: '',
+        state: ''
+    }
+});
+const types = ref([
+    {
+        'name': 'Shipping Address',
+        'value': 'shipping',
+    },
+    {
+        'name': 'Billing Address',
+        'value': 'billing',
+    },
+    {
+        'name': 'Residence Address',
+        'value': 'residence',
+    }
+]);
+let selectedType = ref({});
+const onTypeInput = (selectedItem) => {
+    selectedType.value = selectedItem ? selectedItem : {};
+    form.value.type = selectedItem ? selectedItem.value : '';
+};
+const countries = ref([
+    {
+        'name': 'United States',
+        'value': 'US',
+    },
+    {
+        'name': 'Canada',
+        'value': 'CA',
+    },
+    {
+        'name': 'United Kingdom',
+        'value': 'GB',
+    },
+    {
+        'name': 'India',
+        'value': 'IN',
+    }
+]);
+let selectedCountry = ref({});
+const onCountryInput = (selectedItem) => {
+    selectedCountry.value = selectedItem ? selectedItem : {};
+    form.value.address.country = selectedItem ? selectedItem.value : '';
+};
+const handleSubmit = () => {
+    if (props.addressInfo) {
+        updateAddress();
+    } else {
+        addAddress();
+    }
+};
+const submitDetails = ref({
+    showSubmitButton: true,
+    title: 'Save changes'
+});
+const emit = defineEmits(['add', 'update']);
+function addAddress() {
+    emit('add', form.value);
+};
+function updateAddress() {
+    emit('update', form.value);
+};
+</script>
--- src/components/profile/settings/contact-info.vue
+++ src/components/profile/settings/contact-info.vue
@@ -1,31 +1,39 @@
 <template>
     <div class="m-auto w-100">
         <div class="d-flex">
-            <h5 class="m-0 flex-grow-1 align-self-center">Contact Info</h5>
-            <GenericModal modalId="contact-info-modal" title="contact">
-                <template #activation-button>
-                    <PhPencil size="30" color="#ff5e45" />
-                </template>
-                <template #content>
-                    <p>Hello from the modal!</p>
-                </template>
-            </GenericModal>
+            <h5 class="m-0 flex-grow-1 align-self-center">contact</h5>
+            <button class="btn align-self-center m-0 p-0 me-2" @click="showModal">
+                <PhPencil size="30" color="#ff5e45" />
+            </button>
         </div>
         <div class="pe-2">
             <div v-for="(info, index) in formattedContactInfo" :key="index">
-                <h6>{{ info.type }}</h6>
                 <div class="d-flex gap-1 mb-1 align-items-start">
-                    <div>{{ valueToDisplay(index) }}</div>
-                    <h5>{{ info.isPrimary }}</h5>
-                    <button class="btn m-0 p-0 ms-auto lh-1" @click="toggleMaskedStatus(index)">
-                        <PhEye :size="16" color="#ff5e45" v-show="info.isMasked"/>
-                        <PhEyeSlash :size="16" color="#ff5e45" v-show="!info.isMasked"/>
-                    </button>
+                    <div class="d-flex flex-column">
+                        <h6>{{ info.type }}</h6>
+                        <div class="d-flex gap-1">
+                            <div>{{ valueToDisplay(index) }}</div>
+                            <h5>{{ info.isPrimary }}</h5>
+                        </div>
+                    </div>
+                    <div class="d-flex ms-auto my-auto me-0">
+                        <ContactVerificationModal :contactForm="info" :index="index" v-if="!info.isVerified">
+                            <div class="my-auto mx-0 align-self-center ms-1 me-2">
+                                <PhWarning :size="24" color="#f31b65" weight="fill"/>
+                            </div>
+                        </ContactVerificationModal>
+                        <button class="btn m-0 p-0 lh-1" @click="toggleMaskedStatus(index)">
+                            <PhEye :size="20" color="#ff5e45" v-show="info.isMasked"/>
+                            <PhEyeSlash :size="20" color="#ff5e45" v-show="!info.isMasked"/>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
         <div class="d-flex justify-content-center">
-            <button type="button" class="btn btn-outline-primary">Add New</button>
+            <ContactModal @add="addContact">
+                <span>Add New</span>
+            </ContactModal>
         </div>
     </div>
 
@@ -33,8 +41,9 @@
 
 <script setup>
 import { reactive, computed } from 'vue'
-import { PhPencil, PhEye, PhEyeSlash } from "@phosphor-icons/vue";
-import GenericModal from '../../generic-modal.vue';
+import { PhPencil, PhEye, PhEyeSlash, PhWarning } from "@phosphor-icons/vue";
+import ContactModal from './contact-modal.vue';
+import ContactVerificationModal from './contact-verification-modal.vue';
 const maskedInfo = (value, type) => {
     if (type === 'email') {
         let maskedEmail = "";
@@ -63,19 +72,22 @@ const contactInfo =  [
         type: 'email',
         value: 'patricia.hale@gmail.com',
         isPrimary: true,
-        isMasked: true
+        isMasked: true,
+        isVerified: true
     },
     {
         type: 'email',
         value: 'patricia.hale@yahoo.com',
         isPrimary: false,
-        isMasked: true
+        isMasked: true,
+        isVerified: true
     },
     {
         type: 'phone',
         value: '+1 123 456 7890',
         isPrimary: true,
-        isMasked: true
+        isMasked: true,
+        isVerified: true
     }
 ]
 const formattedContactInfo = reactive(contactInfo.map((info) => {
@@ -110,5 +122,27 @@ const valueToDisplay = computed(() => {
 });
 const toggleMaskedStatus = (index) => {
     formattedContactInfo[index].isMasked = !formattedContactInfo[index].isMasked;
+};
+const addContact = (contact) => {
+    if (contact.type && contact.value) {
+        formattedContactInfo.push({
+            type: contact.type === 'email' ? 'Email Address' : 'Phone Number',
+            value: contact.value,
+            isMasked: true,
+            isVerified: false,
+            maskedValue: maskedInfo(contact.value, contact.type)
+        })
+    }
+};
+const updateContact = (contact, index) => {
+    if (contact.type && contact.value) {
+        formattedContactInfo[index] = {
+            type: contact.type === 'email' ? 'Email Address' : 'Phone Number',
+            value: contact.value,
+            isMasked: true,
+            isVerified: false,
+            maskedValue: maskedInfo(contact.value, contact.type)
+        };
+    }
 };
 </script>
